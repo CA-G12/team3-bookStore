c803ae67270384b8f5d9e9d7911f22c1d82bbb93
husamkamal <husamkmal94@gmail.com>
2022-08-31 15:35:03 +0300

    get cart from database and put header in cart page relates #35

--- server/database/queries/getCart.js
+++ server/database/queries/getCart.js
@@ -1,6 +1,7 @@
 const connection = require("../config/connection");
 
 const getCartQueries = (id)=>{
-    return connection.query(`SELECT * From books join carts on  books.id = carts.bookId  where carts.userId = 2 ` )
+    console.log(id)
+    return connection.query(`SELECT * From books join carts on  books.id = carts.bookId  where carts.userId = $1 `,[id] )
 }
 module.exports = getCartQueries
